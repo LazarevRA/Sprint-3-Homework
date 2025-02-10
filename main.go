349bebeb7d27653f4642297075b5fd6d7f62bed3
Rostislav Lazarev <lazarev.rost.a@gmail.com>
2025-02-10 19:40:42 +0300

    Add 3 task

--- main.go
+++ main.go
@@ -2,45 +2,51 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sort" // или "slices"
 )
 
-// Average возвращает среднее арифметическое элементов слайса []int.
-// Напишите код функции
-// ...
-func Average(list []int) float64 {
-	if len(list) == 0 {
-		return 0
-	}
-	sum := 0
-	count := 0
-	for _, ch := range list {
-		sum += ch
-		count++
-	}
-
-	return float64(sum) / float64(count)
-}
-
-// Range возвращает размах числовой последовательности.
+// Mode возвращает моды числовой последовательности.
 // Напишите код функции
 // ...
 
-func Range(list []int) int {
+func Mode(list []int) ([]int, int) {
 	if len(list) == 0 {
-		return 0
+		return nil, 1
 	}
-	max := list[0]
-	min := max
-	for _, ch := range list {
-		if ch > max {
-			max = ch
-		}
-		if ch < min {
-			min = ch
+	sorted := list
+	sort.Ints(sorted)
+	var result []int
+	colvoChisel := make(map[int]int)
+	for _, ch := range sorted {
+		_, ok := colvoChisel[ch]
+		if !ok {
+			colvoChisel[ch] = 1
+		} else {
+			colvoChisel[ch]++
 		}
 	}
-	return max - min
+
+	max := 0
+	for _, value := range colvoChisel {
+		if value > max {
+			max = value
+		}
+	}
+
+	for key, colvo := range colvoChisel {
+		if colvo == max {
+			result = append(result, key)
+		}
+	}
+
+	if max <= 1 {
+		return nil, max
+	}
+	sortedRes := result
+	sort.Ints(sortedRes)
+
+	return sortedRes, max
+
 }
 
 func main() {
@@ -54,19 +60,28 @@ func main() {
 		{82, -23, 1, 5, 98, 100},
 		{100000, 90000, 20000, 20000, 20000, 22000, 25500, 22000},
 	}
-	averages := []float64{
-		0, 57, 36, 100, 4, 47, 44, 39938,
+	modes := [][]int{
+		{}, {}, {}, {},
+		{4},
+		{-7, 102}, {},
+		{20000},
 	}
-	ranges := []int{
-		0, 0, 85, 200, 1, 109, 123, 80000,
+	mcount := []int{
+		1, 1, 1, 1, 3, 2, 1, 3,
 	}
-
 	for i, list := range lists {
-		if average := math.Round(Average(list)); average != averages[i] {
-			fmt.Printf("average %d: %.2f != %.2f\n", i, averages[i], average)
+		mode, count := Mode(list)
+		if len(mode) != len(modes[i]) {
+			fmt.Printf("len mode %d: %v != %v\n", i, modes[i], mode)
+		} else {
+			for j, v := range mode {
+				if v != modes[i][j] {
+					fmt.Printf("mode %d: %v != %v\n", i, modes[i], mode)
+				}
+			}
 		}
-		if r := Range(list); r != ranges[i] {
-			fmt.Printf("range %d: %d != %d\n", i, ranges[i], r)
+		if count != mcount[i] {
+			fmt.Printf("mcount %d: %d != %d\n", i, mcount[i], count)
 		}
 	}
 	fmt.Println("Тестирование завершено")
